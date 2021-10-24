f5194261e66a16aa405e00db4dc001533997e40e
alanaudi <alanaudi.tw@gmail.com>
2021-10-24 11:26:10 +0800

    config(nvim): change hop keys

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -82,7 +82,7 @@ return require'packer'.startup(function(use) -----------------------------------
     as = 'hop',
     config = function()
       -- you can configure Hop the way you like here; see :h hop-config
-      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+      require'hop'.setup { keys = 'weruioasdfghjklxcvbnm' }
     end
   }
 
